61eff8381ed0e0533595f31bd2f7c2d9dc2103c1
Alex Nunes <alexgaribalde_tb@hotmail.com>
2023-03-13 05:19:57 -0300

    criado classe mentoria e curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso = new Curso();
+        curso.setTitulo("curso Java");
+        curso.setDescricao(" descrição do curso");
+        curso.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descrição da mentoria");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso);
+        System.out.println(mentoria);
     }
 }
